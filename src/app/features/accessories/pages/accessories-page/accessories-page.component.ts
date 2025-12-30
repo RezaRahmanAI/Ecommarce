145d476cebbdfe9f03cda823bed1abc83d022bd0
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2025-12-30 08:53:14 +0600

    Add accessories page

---
+++ src/app/features/accessories/pages/accessories-page/accessories-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-accessories-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './accessories-page.component.html',
+  styleUrl: './accessories-page.component.css',
+})
+export class AccessoriesPageComponent {}
